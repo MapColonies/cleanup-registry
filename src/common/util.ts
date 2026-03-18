49d3a24e1163d93eb42df13316cbc94cf0bbeb42
eyal rozen <eyalr1100@gmail.com>
2026-03-18 18:19:19 +0200

    fix: fix use of timer

--- src/common/util.ts
+++ src/common/util.ts
@@ -10,7 +10,7 @@ export const promiseResult = async <T>(promise: Promise<T>): Promise<[undefined,
 };
 
 export const promiseTimeout = async <T>(promise: Promise<T>, ms: number): Promise<T> => {
-  let timer: NodeJS.Timer;
+  let timer: ReturnType<typeof setTimeout> | undefined;
 
   const timeout = new Promise<T>((_, reject) => {
     timer = setTimeout(() => {
