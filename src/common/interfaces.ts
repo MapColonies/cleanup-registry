1f73702a79164cb13fb9a3a68801b868b937ff1e
eyal rozen <eyalr1100@gmail.com>
2026-03-22 16:52:48 +0200

    fix: decouple from mapcolonies logger

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,11 +1,23 @@
-import { Logger } from '@map-colonies/js-logger';
 import { AsyncFunc, ItemId, FinishStatus } from './types';
+
+export interface LogFn {
+  (obj: unknown, msg?: string, ...args: unknown[]): void;
+  (msg: string, ...args: unknown[]): void;
+}
+
+export interface ILogger {
+  debug: LogFn;
+  info: LogFn;
+  warn: LogFn;
+  error: LogFn;
+  fatal?: LogFn;
+}
 
 export interface RegistryOptions {
   preCleanupHook?: AsyncFunc;
   postCleanupHook?: AsyncFunc;
   overallTimeout?: number;
-  logger?: Logger;
+  logger?: ILogger;
 }
 
 export interface TriggerOptions {
